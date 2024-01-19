ef0b170a5a2582df6e9f3c25b497b5620f39d045
mourav2222 <mourav2222@gmail.com>
2024-01-19 17:14:53 +0100

    Initial commit

---
+++ src/test/java/mkm/example/testcontainersdemo/TestcontainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mkm.example.testcontainersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
